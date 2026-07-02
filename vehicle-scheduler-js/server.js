b7378efb1ed4016ec6ea05562c5daa48c489dd9e
Pooja M <itsmepooja45@gmail.com>
2026-07-02 12:45:36 +0530

    Set up Express server with rate limiting

--- vehicle-scheduler-js/server.js
+++ vehicle-scheduler-js/server.js
@@ -1,1 +1,16 @@
 
+const express= require('express');
+require("dotenv").config();
+
+const rateLimiter=require('./middleware/rateLimiter')
+const app=express();
+
+app.use(express.json())
+app.use(rateLimiter)
+
+app.get('/depots',(req,res)=>{ return res.json(["ID","MechanicHours"])})
+app.get('/vehicles',(req,res)=>{return res.json(["TaskID","Duration","Impact"])})
+
+app.listen(process.env.PORT,()=>{console.log(`Server is running on port ${process.env.PORT}`)})
+
+
